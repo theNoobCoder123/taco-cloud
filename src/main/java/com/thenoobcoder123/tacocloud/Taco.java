20528a2032de348082e0c8aa9c2e660ddf0c128d
theNoobCoder123 <patelhrishikesh2000@gmail.com>
2021-10-26 00:15:13 +0530

    Added project files

---
+++ src/main/java/com/thenoobcoder123/tacocloud/Taco.java
@@ -0,0 +1,7 @@
+package com.thenoobcoder123.tacocloud;
+
+import java.util.List;
+
+public class Taco {
+    public List<Ingredient> ingredients;
+}
---
+++ src/main/java/com/thenoobcoder123/tacocloud/TacoCloudApplication.java
@@ -0,0 +1,11 @@
+package com.thenoobcoder123.tacocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TacoCloudApplication.class, args);
+	}
+}
