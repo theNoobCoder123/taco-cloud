f367d7a3cb690b1543786766027cc46d29ce6af0
theNoobCoder123 <patelhrishikesh2000@gmail.com>
2021-11-24 00:06:48 +0530

    Added id and date to Taco and Order classes.

--- src/main/java/com/thenoobcoder123/tacocloud/Taco.java
+++ src/main/java/com/thenoobcoder123/tacocloud/Taco.java
@@ -1,5 +1,6 @@
 package com.thenoobcoder123.tacocloud;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.constraints.NotBlank;
@@ -10,11 +11,15 @@ import lombok.Data;
 
 @Data
 public class Taco {
+    private Long id;
+
     @NotBlank
     @Size(min=5, message="Name can't be less than 5 letters.")
-    public String name;
+    private String name;
 
     @NotNull
     @Size(min=1, message="You must choose atleast 1 ingredient.")
-    public List<String> ingredients;
+    private List<String> ingredients;
+
+    private Date createdAt;
 }
